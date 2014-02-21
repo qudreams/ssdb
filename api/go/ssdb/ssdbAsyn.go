0e3626c2adb609efc6d389f777e162f164a9bd88
屈梦 <qudreams123@163.com>
2014-02-21 22:31:49 +0800

    Create ssdbAsyn.go
    asynchronous client api fro SSDB.

---
+++ api/go/ssdb/ssdbAsyn.go
@@ -0,0 +1,361 @@
+/*
+ *ssdb asynchronous API : Copyright to qudreams(2014)
+ *All rights reserved.
+ *Note:
+ *It depend on ssdb synchronous client.
+ *You can find more detailed documention about SSDB protocol at
+ * http://www.ideawu.com/ssdb
+ */
+
+package ssdb
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+type CntlCode byte
+
+const (
+	_ CntlCode = iota
+	start
+	stop
+)
+
+type StatusCode byte
+
+const (
+	_ StatusCode = iota
+	OK
+	NOT_FOUND
+	CLIENT_ERR
+	FAIL
+	ERROR
+	UNKNOWN
+)
+
+const (
+	DefaultReqChanCap = 4096
+	DefaultRspChanCap = 4096
+)
+
+type SsdbAsynReply struct {
+	Err    error
+	Status StatusCode
+	Reply  []string
+}
+
+type responseCallback func(*SsdbAsynReply, *SsdbAsynClient)
+
+type SsdbAsynRequest struct {
+	packet   bytes.Buffer
+	callback responseCallback
+}
+
+type SsdbAsynClient struct {
+	client         *Client
+	requestsQueue  chan *SsdbAsynRequest
+	responsesQueue chan *SsdbAsynRequest
+	reqsCntl       chan CntlCode
+	respsCntl      chan CntlCode
+	isShutdown     bool
+	shutdown       chan bool
+	mutex          sync.Mutex
+}
+
+func newSsdbAsynClient() (*SsdbAsynClient, error) {
+	asynClient := new(SsdbAsynClient)
+	if asynClient == nil {
+		return nil, errors.New("newSsdbAsynclient: out of memory to allocate memory")
+	}
+
+	client := new(Client)
+	if client == nil {
+		return nil, errors.New("newSsdbAsynclient: out of memory to allocate memory")
+	}
+
+	asynClient.client = client
+	asynClient.requestsQueue = make(chan *SsdbAsynRequest, DefaultReqChanCap)
+	asynClient.responsesQueue = make(chan *SsdbAsynRequest, DefaultRspChanCap)
+	asynClient.reqsCntl = make(chan CntlCode, 1)
+	asynClient.respsCntl = make(chan CntlCode, 1)
+	asynClient.shutdown = make(chan bool, 1)
+	asynClient.isShutdown = false
+
+	return asynClient, nil
+}
+
+func procAsynRequests(asynClient *SsdbAsynClient) {
+	defer func() {
+		if re := recover(); re != nil {
+			err := re.(error)
+			panic("proccess asynchronous request " + err.Error())
+		}
+	}()
+
+	select {
+	case cntl := <-asynClient.reqsCntl:
+		if cntl == stop {
+			close(asynClient.responsesQueue)
+			return
+		}
+	}
+
+	for {
+		select {
+		case cntl := <-asynClient.reqsCntl:
+			if cntl == stop {
+				close(asynClient.responsesQueue) //Note: sender close the channel
+				return
+			}
+		case req, ok := <-asynClient.requestsQueue:
+			if ok {
+				err := asynClient.sendRequest(req)
+				if err != nil {
+					panic(err.Error())
+				}
+				asynClient.responsesQueue <- req
+			}
+		}
+	}
+}
+
+func procAsynResponses(asynClient *SsdbAsynClient) {
+	defer func() {
+		if re := recover(); re != nil {
+			err := re.(error)
+			panic("process asynchronous response " + err.Error())
+		}
+	}()
+
+	select {
+	case cntl := <-asynClient.respsCntl:
+		if cntl == stop {
+			return
+		}
+	}
+
+	for {
+		select {
+		case cntl := <-asynClient.respsCntl:
+			if cntl == stop {
+				return
+			}
+		case rsp, ok := <-asynClient.responsesQueue:
+			if ok {
+				rep := asynClient.recvResponse()
+				rsp.callback(rep, asynClient)
+			}
+		}
+	}
+}
+
+func (asynClient *SsdbAsynClient) startup() error {
+	defer func() {
+		if re := recover(); re != nil {
+			err := re.(error)
+			panic("SsdbAsyn: failed to start up" + err.Error())
+		}
+	}()
+
+	go procAsynRequests(asynClient)
+	asynClient.reqsCntl <- start
+	go procAsynResponses(asynClient)
+	asynClient.respsCntl <- start
+
+	return nil
+}
+
+//connect SSDB server without timeout
+//Note: it just support IPv4
+func SsdbAsynConnect(ip string, port int) (*SsdbAsynClient, error) {
+	addr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", ip, port))
+	if err != nil {
+		return nil, fmt.Errorf("SsdbAsyn: failed to parse server address %s", err.Error())
+	}
+
+	asynClient, err := newSsdbAsynClient()
+	if err != nil {
+		return nil, err
+	}
+
+	sock, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return nil, fmt.Errorf("SsdbAsyn: %s", err.Error())
+	}
+
+	client := asynClient.client
+	client.sock = sock
+
+	err = asynClient.startup()
+
+	return asynClient, err
+}
+
+func SsdbAsynConnectWithTimeout(ip string, port int, sec time.Duration) (*SsdbAsynClient, error) {
+	asynClient, err := newSsdbAsynClient()
+	if err != nil {
+		return nil, err
+	}
+
+	addr := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", addr, sec*time.Second)
+	if err != nil {
+		return nil, fmt.Errorf("SsdbAsyn: %s", err.Error())
+	}
+
+	tcpConn, ok := conn.(*net.TCPConn)
+	if !ok {
+		conn.Close()
+		return nil, fmt.Errorf("SsdbAsyn: type assert failed from net.Conn to net.TCPConn")
+	}
+
+	client := asynClient.client
+	client.sock = tcpConn
+
+	err = asynClient.startup()
+
+	return asynClient, err
+}
+
+//ssdb asynchronous disconect:
+//close request channel
+func (asynClient *SsdbAsynClient) SsdbAsynDisconnect() {
+	go func() {
+		defer func() {
+			if re := recover(); re != nil {
+				err := re.(error)
+				panic("Ssdb Asynchronous disconnect " + err.Error())
+			}
+		}()
+
+		asynClient.mutex.Lock()
+		if asynClient.isShutdown == true {
+			asynClient.mutex.Unlock()
+			return
+		}
+		asynClient.isShutdown = true
+		asynClient.mutex.Unlock()
+
+		asynClient.reqsCntl <- stop
+		asynClient.respsCntl <- stop
+		asynClient.shutdown <- true
+
+		/*Note:
+		 *sender close the channel
+		 */
+		close(asynClient.reqsCntl)
+		close(asynClient.respsCntl)
+		close(asynClient.shutdown)
+
+		asynClient.client.Close()
+		asynClient.client = nil
+	}()
+}
+
+func (asynClient *SsdbAsynClient) SsdbAsynSetTimeout(sec time.Duration) {
+	asynClient.client.SetDeadline(sec)
+}
+
+func (asynClient *SsdbAsynClient) Do(callback responseCallback, args ...interface{}) error {
+
+	defer func() {
+		if re := recover(); re != nil {
+			err := re.(error)
+			panic("SsdbAsyn: failed to send request to client " + err.Error())
+		}
+	}()
+
+	select {
+	case shutdown := <-asynClient.shutdown:
+		if shutdown {
+			close(asynClient.requestsQueue) //sender close the channel
+			return fmt.Errorf("connection to SSDB has been closed")
+		}
+	default:
+	}
+
+	req := new(SsdbAsynRequest)
+	req.callback = callback
+
+	var buf *bytes.Buffer = &req.packet
+	for _, arg := range args {
+		var s string
+		switch arg := arg.(type) {
+		case string:
+			s = arg
+		case []byte:
+			s = string(arg)
+		case int:
+			s = fmt.Sprintf("%d", arg)
+		case int64:
+			s = fmt.Sprintf("%d", arg)
+		case float64:
+			s = fmt.Sprintf("%f", arg)
+		case bool:
+			if arg {
+				s = "1"
+			} else {
+				s = "0"
+			}
+		case nil:
+			s = ""
+		default:
+			return fmt.Errorf("bad arguments")
+		}
+		buf.WriteString(fmt.Sprintf("%d", len(s)))
+		buf.WriteByte('\n')
+		buf.WriteString(s)
+		buf.WriteByte('\n')
+	}
+	buf.WriteByte('\n')
+
+	asynClient.requestsQueue <- req
+
+	return nil
+}
+
+func (asynClient *SsdbAsynClient) sendRequest(r *SsdbAsynRequest) error {
+	client := asynClient.client
+
+	_, err := client.sock.Write(r.packet.Bytes())
+
+	return err
+}
+
+func (asyncClient *SsdbAsynClient) recvResponse() *SsdbAsynReply {
+	client := asyncClient.client
+	rep := new(SsdbAsynReply)
+
+	rep.Reply, rep.Err = client.recv()
+	if rep.Err == nil {
+		//we got a reply
+		switch rep.Reply[0] {
+		case "ok":
+			rep.Status = OK
+		case "not_found":
+			rep.Status = NOT_FOUND
+		case "client_error":
+			rep.Status = CLIENT_ERR
+		case "fail":
+			rep.Status = FAIL
+		case "error":
+			rep.Status = ERROR
+		default:
+			rep.Status = UNKNOWN
+		}
+
+		//skip status flag of the reply.
+		if len(rep.Reply) > 1 {
+			rep.Reply = rep.Reply[1:]
+		} else {
+			rep.Reply = []string{}
+		}
+	}
+
+	return rep
+}
